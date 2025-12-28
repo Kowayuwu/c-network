3c12a2fbdbcabe4e5f2ead4644c6c69614243d89
Kowayuwu <brianbrianwu1030@gmail.com>
2025-12-28 23:39:05 +1100

    Successfully parsed IPv4 and TCP packages and extracted needed information
    Project basically completed, I could add IPv6 and UDP structs if I want but we'll see.
    
    I could also change the multiple fread() into one single fread into a variable and read its byte offsets but that's an 'optimization' that I don't think I need to do right now.

--- pcap_analyze.c
+++ pcap_analyze.c
@@ -2,8 +2,29 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+// for ntoh functions, I know I wrote my own byte swapping function but hey using this is cool as well
+#ifdef _WIN32
+#include <winsock2.h>
+#else
+#include <arpa/inet.h>
+#endif
+
 #include "network_package_struct.h"
 #include "pcap_util.h"
+
+/*
+Structure of this pcap file:
+
+pcap file header
+ [
+    pcap packet header (tells us how long the packet is)
+    link layer header (tells us the ip version, for this project we use the loopback interface https://www.tcpdump.org/linktypes/LINKTYPE_NULL.html)
+    IP header (BIG ENDIAN!)
+    TCP header (BIG ENDIAN!)
+    payload
+ ]
+
+*/
 
 int main()
 {
@@ -62,8 +83,16 @@ int main()
     printf("pcap packet time_in_detail is in unit: %s\n", pcap_packet_time_in_microsec ? "Micro-second" : "Nano-second");
 
     int packet_count = 0;
+    int SYN_initiated_count = 0;
+    int ACK_count = 0;
+
+    // size of headers
     const int PCAP_PACKET_HEADER_SIZE = 16;
-    // start reading packets
+    const int LOOPBACK_INTERFACE_LINK_LAYER_HEADER_SIZE = 4;
+    const int IPV4_HEADER_MIN_SIZE = 20;
+    const int TCP_HEADER_MIN_SIZE = 20;
+
+    // start reading packets till end of file
     while (true)
     {
         pcap_packet_header_t pcap_p_hdr;
@@ -95,10 +124,126 @@ int main()
             printf("Found data truncated, stop reading packets\n"); // didn't happen in this file, decide not to handle it in this project
             break;
         }
-        // printf("%d\n", pcap_p_hdr.captured_data_length);
-        fseek(file, (long)pcap_p_hdr.captured_data_length, SEEK_CUR);
+        uint32_t remaining_packet_data_length = pcap_p_hdr.captured_data_length;
+
+        // read link layer header and determine protocal type (should all be ipv4 for this project)
+        loopback_interface_link_layer_header_t link_layer_header;
+        fread(&link_layer_header, LOOPBACK_INTERFACE_LINK_LAYER_HEADER_SIZE, 1, file);
+        remaining_packet_data_length -= LOOPBACK_INTERFACE_LINK_LAYER_HEADER_SIZE;
+
+        enum InternetLayerProtocal internet_protocal_type;
+        internet_protocal_type = (enum InternetLayerProtocal)link_layer_header.protocal_type;
+        switch (internet_protocal_type)
+        {
+        case IPV4:
+        case IPV6_24:
+        case IPV6_28:
+        case IPV6_30:
+        case OSI:
+        case IPX:
+            // valid!
+            break;
+        default:
+            internet_protocal_type = INTERNET_PROTOCAL_ERROR;
+            break;
+        }
+
+        if (internet_protocal_type == INTERNET_PROTOCAL_ERROR)
+        {
+            fprintf(stderr, "Error reading internet protocal type at packet number %d\n", packet_count);
+            fclose(file);
+            return 1;
+        }
+
+        // read internet protocal header, should all be ipv4 in this project so not handling others :D
+        ipv4_header_min_t ipv4_header;
+        switch (internet_protocal_type)
+        {
+        case IPV4:
+            fread(&ipv4_header, IPV4_HEADER_MIN_SIZE, 1, file);
+            remaining_packet_data_length -= IPV4_HEADER_MIN_SIZE;
+            break;
+
+        default:
+            fprintf(stderr, "protocal is not IPV4, should not happen at this current project state\n");
+            fclose(file);
+            return 1;
+            break;
+        }
+
+        // see if ipv4 options exists, if so then we need to read more, NOTE that in ipv4 the IHL unit is in 32 bits not byte
+        uint8_t ihl_in_bytes = (ipv4_header.version_and_IHL & 0x0f) << 2;
+        uint8_t ipv4_options_length = ihl_in_bytes - IPV4_HEADER_MIN_SIZE;
+
+        if (ipv4_options_length > 0)
+        {
+            // don't care about IP options for now
+            fseek(file, (long)ipv4_options_length, SEEK_CUR);
+            remaining_packet_data_length -= ipv4_options_length;
+        }
+
+        // determine tranport layer protocal type, assuming we have ipv4 packet
+        enum TransportLayerProtocal transport_protocal_type;
+        transport_protocal_type = (enum TransportLayerProtocal)ipv4_header.protocal;
+        switch (transport_protocal_type)
+        {
+        case ICMP:
+        case IGMP:
+        case TCP:
+        case UDP:
+        case ENCAP:
+        case OSPF:
+        case SCTP:
+            // valid!
+            break;
+
+        default:
+            transport_protocal_type = TRANSPORT_PROTOCAL_ERROR;
+            break;
+        }
+        if (transport_protocal_type == TRANSPORT_PROTOCAL_ERROR)
+        {
+            fprintf(stderr, "Error reading transport protocal type at packet number %d\n", packet_count);
+            fclose(file);
+            return 1;
+        }
+
+        // read transport protocal header, assuming we have TCP
+        tcp_header_min_t tcp_header;
+        fread(&tcp_header, TCP_HEADER_MIN_SIZE, 1, file);
+        remaining_packet_data_length -= TCP_HEADER_MIN_SIZE;
+
+        // see if TCP header options exists, if so then we need to read more, NOTE that in ipv4 the IHL unit is in 32 bits not byte
+        uint8_t data_offset_in_bytes = (tcp_header.data_offset_reserved & 0xf0) >> 2; // shift right 4 then left 2
+        uint8_t tcp_options_length = data_offset_in_bytes - TCP_HEADER_MIN_SIZE;
+
+        if (tcp_options_length > 0)
+        {
+            // don't care about TCP options for now
+            fseek(file, (long)tcp_options_length, SEEK_CUR);
+            remaining_packet_data_length -= tcp_options_length;
+        }
+
+        // byte swap tcp_header fields (if > 1 byte) because TCP uses BIG Endian
+        uint16_t dst_port = ntohs(tcp_header.destination_port);
+        uint16_t src_port = ntohs(tcp_header.source_port);
+
+        bool SYN_flagged = (tcp_header.flags & 0x0002) != 0;
+        bool ACK_flagged = (tcp_header.flags & 0x0010) != 0;
+
+        if (SYN_flagged && dst_port == 80)
+        {
+            SYN_initiated_count += 1;
+        }
+        if (ACK_flagged && src_port == 80)
+        {
+            ACK_count += 1;
+        }
+
+        fseek(file, (long)remaining_packet_data_length, SEEK_CUR);
     }
-    printf("%d amount of packets read", packet_count);
+    printf("%d amount of packets read\n", packet_count);
+    printf("Initiated SYN count: %d, ACK count: %d\n", SYN_initiated_count, ACK_count);
 
     fclose(file);
     return 0;
